e29ff0f4aadf770be248fdd7e66f1c6e7fa014fb
da0shi <whit3.develop@gmail.com>
2015-06-08 23:46:52 +0900

    Load config file when argument was 'init'
    `cmail init` とした時に引数がどうやって取得できるか？
    json ファイルを読み込んで config 変数に代入する

--- main.js
+++ main.js
@@ -6,3 +6,8 @@ console.log('Hello Cmail!');
 console.log(process.argv);
 
 console.log(process.env.HOME);
+
+if (/* 引数が init だったら*/) {
+  var config = /* 'scripts-to-ignore/config.json' を読み込み*/;
+  console.log(config);
+}
