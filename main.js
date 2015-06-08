8ab48ba98b74c1cc6b27796fa7fa72216b6bf70b
da0shi <whit3.develop@gmail.com>
2015-06-08 21:08:35 +0900

    Answer for section 01

--- main.js
+++ main.js
@@ -5,8 +5,4 @@ console.log('Hello Cmail!');
 
 console.log(process.argv);
 
-/**
- * 環境変数から HOME の中身を表示してみよう
- * コンソールへの出力は `console.log("出力内容")`
- * ヒント: `process` でプロセスに関わる値にアクセスができる
- */
+console.log(process.env.HOME);
