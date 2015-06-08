85ddcdfa8335c1ceda7b9949620972b44ced4fdb
da0shi <whit3.develop@gmail.com>
2015-06-08 20:52:17 +0900

    npm init & sample command script
    npm init でパッケージの作成
    package.json を編集してコマンドラインツール化に対応
    npm link でコマンドラインツール化
    node.js を使った出力を試してコマンドが実行できる事を確認

---
+++ main.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+'use strict';
+
+console.log('Hello Cmail!');
+
+console.log(process.argv);
+
+/**
+ * 環境変数から HOME の中身を表示してみよう
+ * コンソールへの出力は `console.log("出力内容")`
+ * ヒント: `process` でプロセスに関わる値にアクセスができる
+ */
