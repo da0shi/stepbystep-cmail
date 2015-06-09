fcde6935b18388cc8ae234a92bddad006f8ec1ac
da0shi <whit3.develop@gmail.com>
2015-06-09 23:52:45 +0900

    Answer for section 04

--- main.js
+++ main.js
@@ -25,10 +25,6 @@ if (process.argv[2] === 'init') {
     state: 'some random string for security'
   };
   var uri = config.auth_uri +'?'+ querystring.encode(params);
-  /**
-   * open モジュールは URI を指定すると
-   * デフォルトに設定されてるブラウザで URI を開いてくれる
-   */
   require('open')(uri);
 
   var code = readline.question('Input returned code: ');
@@ -36,27 +32,22 @@ if (process.argv[2] === 'init') {
     client_id: config.client_id,
     client_secret: config.client_secret,
     redirect_uri: config.redirect_uris[0],
-    /**
-     * ここのパラメータで必要になる残り 2 項目を埋めてください
-     * 片方は上で得た変数 `code` を活用します
-     */
+    grant_type: 'authorization_code',
+    code: code
   };
   var options = {
     uri: config.token_uri,
-    /**
-     * request モジュールで POST を使ってパラメータを送る場合の
-     * option のキーを調べてみよう
-     */
+    form: params,
     json: true
   };
 
   request.post(options, function (error, response, body) {
-    if (/* レスポンスの HTTP Status Code が OK でなければ */) {
+    if (response.statusCode !== 200) {
       console.log("Error: ", error);
       console.log("HTTP Status Code: ", response.statusCode);
       console.log("Body: ", body);
       return false;
     }
-    fs.writeFileSync('./scripts-to-ignore/token.json', /* body オブジェクトを Json 文字列に変換 */);
+    fs.writeFileSync('./scripts-to-ignore/token.json', JSON.stringify(body));
   });
 }
