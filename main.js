cf8b154dc07da3ea3e90f9e744c180a77663a5f7
da0shi <whit3.develop@gmail.com>
2015-06-09 23:42:19 +0900

    Get access_token and save to token.json
    アクセストークンの取得に必要なパラメータの調査
    request モジュールでのパラメータの指定方法
    response と HTTP Status Code
    JavaScript の Object を  json 文字列に変換

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 'use strict';
 
 var querystring = require('querystring');
+var readline = require('readline-sync');
+var request = require('request');
+var fs = require('fs');
 
 console.log('Hello Cmail!');
 
@@ -22,5 +25,38 @@ if (process.argv[2] === 'init') {
     state: 'some random string for security'
   };
   var uri = config.auth_uri +'?'+ querystring.encode(params);
-  console.log(uri);
+  /**
+   * open モジュールは URI を指定すると
+   * デフォルトに設定されてるブラウザで URI を開いてくれる
+   */
+  require('open')(uri);
+
+  var code = readline.question('Input returned code: ');
+  var params = {
+    client_id: config.client_id,
+    client_secret: config.client_secret,
+    redirect_uri: config.redirect_uris[0],
+    /**
+     * ここのパラメータで必要になる残り 2 項目を埋めてください
+     * 片方は上で得た変数 `code` を活用します
+     */
+  };
+  var options = {
+    uri: config.token_uri,
+    /**
+     * request モジュールで POST を使ってパラメータを送る場合の
+     * option のキーを調べてみよう
+     */
+    json: true
+  };
+
+  request.post(options, function (error, response, body) {
+    if (/* レスポンスの HTTP Status Code が OK でなければ */) {
+      console.log("Error: ", error);
+      console.log("HTTP Status Code: ", response.statusCode);
+      console.log("Body: ", body);
+      return false;
+    }
+    fs.writeFileSync('./scripts-to-ignore/token.json', /* body オブジェクトを Json 文字列に変換 */);
+  });
 }
